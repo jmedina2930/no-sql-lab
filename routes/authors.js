961ddb45560f62d37435778f6ff7c533ca3225eb
NeiverTapia <neiver.escobar569@gmail.com>
2020-05-03 13:13:49 -0500

    Solucion de trabajo base de datos

--- routes/authors.js
+++ routes/authors.js
@@ -4,13 +4,63 @@ const Author = require('../models/author');
 const router = express.Router();
 
 /**
- * GET users listing.
+ * GET Authors listing.
  */
 router.get('/', async (req, res) => {
   try {
     let filters = {};
     if (req.query.pais) filters = { pais: req.query.pais };
     const authors = await Author.find(filters);
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+/*
+Realizar los métodos (Endpoints) 
+adicionales para las siguientes consultas:
+Primer punto del taller
+*/
+router.get('/publicaciones-mayores-20', async (req, res) => {
+  try {
+    let filters = {};
+    console.log(req.query);
+    if (req.query.pais) filters = { pais: req.query.pais };
+    const authors = await Author.find({publicados: {$gte:20}, pais:'Colombia'});
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/*
+Segunto punto del taller
+Consultar los nombres de los autores que 
+no tengan apellido en la base de datos.
+*/
+router.get('/sin-apellido', async(req, res)=>{
+  try{
+    let filters={}
+    console.log(req.query);
+    if(req.query.pais) filters= {pais: req.query.pais}
+    const authors= await Author.find({apellido: ''});
+    res.json(authors);
+  }catch(err){
+    res.status(500).json({message: err.message});
+  }
+});
+
+/*
+Tercer punto del taller
+Consultar los apellidos de los autores que tengan
+ menos de 20 publicaciones o que su país sea Argentina.
+ */
+ router.get('/publicaciones-menores-20', async (req, res) => {
+  try {
+    let filters = {};
+    console.log(req.query);
+    if (req.query.pais) filters = { pais: req.query.pais };
+    const authors = await Author.find({publicados: {$lte:20}, pais:'Argentina'});
     res.json(authors);
   } catch (err) {
     res.status(500).json({ message: err.message });
