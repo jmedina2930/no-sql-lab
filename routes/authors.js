7fdb079e09c3dee858c53900d88a697b4dc647f2
DanielaMGX <39426781+DanielaMGX@users.noreply.github.com>
2020-07-24 22:00:08 -0500

    Lab 6
    Desarrollo de los puntos 1, 2 y 3

--- routes/authors.js
+++ routes/authors.js
@@ -18,6 +18,50 @@ router.get('/', async (req, res) => {
 });
 
 /**
+ * Consultar los nombres y los apellidos de los autores que tengan 
+ * publicaciones superiores o iguales a 20 y su país sea Colombia.
+ * 
+ */
+
+router.get('/punto1', async (req, res) => {
+  try {
+    const authors = await Author.find({ pais : 'Colombia', publicados:{$gte:20}}, {nombre:1, apellido:2} );
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * 
+ * Consultar los nombres de los autores que no tengan apellido en la base de datos.
+ */
+
+router.get('/punto2', async (req, res) => {
+  try {
+    const authors = await Author.find({ apellido : null}, {nombre:1} );
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * Consultar los apellidos de los autores que tengan menos 
+ * de 20 publicaciones o que su país sea Argentina.
+ * 
+ * */ 
+
+router.get('/punto3', async (req, res) => {
+  try {
+    const authors = await Author.find({$or: [{pais : 'Argentina'},  {publicados:{$lt:20}}] }, {apellido:1} );
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
  * Create a new Author
  */
 router.post('/', async (req, res) => {
