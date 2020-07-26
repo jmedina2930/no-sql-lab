8c358bd36d236a1ff3fb9641b9b138db5a0b9ee3
MairaRodas <mrodas011@gmail.com>
2020-07-26 17:38:15 -0500

    Se agregaron autores y endpoints

--- routes/authors.js
+++ routes/authors.js
@@ -30,4 +30,46 @@ router.post('/', async (req, res) => {
   }
 });
 
+/**
+ * Consultar los nombres y los apellidos de los autores que tengan publicaciones superiores o iguales a 20 y su país sea Colombia.
+ */
+router.get('/sup20Colombia', async (req, res) => {
+  try {
+    let filters = {pais: "Colombia", publicados:{$gt:20}};
+    let projections = {nombre:1, apellido:1, _id:0};
+    const authors = await Author.find(filters, projections);
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * Consultar los nombres de los autores que no tengan apellido en la base de datos.
+ */
+router.get('/sinApellido', async (req, res) => {
+  try {
+    let filters = {apellido: null};
+    let projections = {nombre:1, _id:0};
+    const authors = await Author.find(filters, projections);
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * Consultar los apellidos de los autores que tengan menos de 20 publicaciones o que su país sea Argentina.
+ */
+router.get('/inf20orArgentina', async (req, res) => {
+  try {
+    let filters ={$or:[{publicados:{$lt:20}},{pais: "Argentina"}]};
+    let projections = {apellido:1, _id:0};
+    const authors = await Author.find(filters, projections);
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = router;
