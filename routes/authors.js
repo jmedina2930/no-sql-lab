05d5520ac114ba7c74961208d5ea93b2a8418a4a
krisg23 <cristhian.gonzalez@udea.edu.co>
2020-07-26 00:17:04 -0500

    Laboratorio Resuelto

--- routes/authors.js
+++ routes/authors.js
@@ -18,6 +18,50 @@ router.get('/', async (req, res) => {
 });
 
 /**
+ * ===============================================================================
+ * Consultar los nombres y los apellidos de los autores que tengan publicaciones 
+ * superiores o iguales a 20 y su país sea Colombia.
+ * ===============================================================================
+ */
+router.get('/authors20col', async (req, res) => {
+  try {
+    const authors = await Author.find({ $and: [ { publicados: { $gte: 20 } }, { pais: 'Colombia' } ] }, 'nombre apellido');
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * ================================================================================
+ * Consultar los nombres de los autores que no tengan apellido en la base de datos.
+ * ================================================================================
+ */
+router.get('/sin-apellido', async (req, res) => {
+  try {
+    const authors = await Author.find({ apellido: undefined }, 'nombre' );
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * ============================================================================
+ * Consultar los apellidos de los autores que tengan menos de 20 publicaciones 
+ * o que su país sea Argentina.
+ * ============================================================================
+ */
+router.get('/authors20arg', async (req, res) => {
+  try {
+    const authors = await Author.find({ $or: [ { publicados: { $lt: 20 } }, { pais: 'Argentina' } ] }, 'apellido');
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
  * Create a new Author
  */
 router.post('/', async (req, res) => {
