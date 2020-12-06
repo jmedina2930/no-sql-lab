c22eada84971e96c24ccd58a4ef2340ff8f50ca9
Luis Hincapié <lmateo.hincapie@udea.edu.co>
2020-12-06 01:24:36 -0500

    Three empoints are added

--- routes/authors.js
+++ routes/authors.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const Author = require('../models/author');
+const express = require("express");
+const Author = require("../models/author");
 
 const router = express.Router();
 
 /**
  * GET authors listing.
  */
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     let filters = {};
     if (req.query.pais) filters = { pais: req.query.pais };
@@ -18,9 +18,70 @@ router.get('/', async (req, res) => {
 });
 
 /**
+ * GET authors listing by "nombre" and "apellido"
+ * WHEN "publicaciones" < OR = 20 AND "pais" = "Colombia".
+ */
+router.get("/query1", async (req, res) => {
+  try {
+    const authors = await Author.find(
+      {
+        $and: [{ publicados: { $lte: 20 } }, { pais: "Colombia" }],
+      },
+      {
+        nombre: 1,
+        _id: 0,
+      }
+    );
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * GET authors listing by "nombre"
+ * WHEN EXIST "apellido".
+ */
+router.get("/query2", async (req, res) => {
+  try {
+    const authors = await Author.find(
+      { apellido: { $exists: true } },
+      {
+        nombre: 1,
+        _id: 0,
+      }
+    );
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * GET authors listing by "apellido"
+ * WHEN "publicaciones" > 20 OR "pais" = "Argentina".
+ */
+router.get("/query3", async (req, res) => {
+  try {
+    let filters = {
+      $and: [
+        { apellido: { $exists: true } },
+        {
+          $or: [{ publicados: { $gt: 20 } }, { pais: "Argentina" }],
+        },
+      ],
+    };
+    const authors = await Author.find(filters, { apellido: 1, _id: 0 });
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
  * Create a new Author
  */
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     let author = new Author(req.body);
     author = await author.save({ new: true });
