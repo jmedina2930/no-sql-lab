2b27b922db683c584b877385992111509e021d39
Andres Higuita <adario.higuita@udea.edu.co>
2020-12-06 00:41:41 -0500

    Agregacion de endpoints
    Andres Higuita ha realizado las inserciones y realizado los 3 endpoint a realizar.

--- routes/authors.js
+++ routes/authors.js
@@ -17,6 +17,37 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/consulta1', async (req, res) => {
+  try {
+    let filters = {};
+    if (req.query.pais) filters = { pais: req.query.pais };
+    const authors = await Author.find({pais: "Colombia", publicados: {$lte: 20}}, {nombre:1, apellido:1, _id:0});
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+router.get('/consulta2', async (req, res) => {
+  try {
+    let filters = {};
+    if (req.query.pais) filters = { pais: req.query.pais };
+    const authors = await Author.find({apellido: {$ne: null}}, {nombre:1, _id:0});
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+router.get('/consulta3', async (req, res) => {
+  try {
+    let filters = {};
+    if (req.query.pais) filters = { pais: req.query.pais };
+    const authors = await Author.find({$or: [{publicados: {$gt: 20}}, {pais: "Argentina"}]},{apellido:1, _id:0});
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 /**
  * Create a new Author
  */
