5a2d9a1ab6d5393bef1b2fa4517bc6c8b0948a00
leonel1221 <lzm_21@hotmail.com>
2020-12-06 13:29:03 -0500

    Laboratorio no-sql realizado por Alcides Zapata Méndez CC 8055453 && Leonel Zapata Méndez CC 103810562

--- routes/authors.js
+++ routes/authors.js
@@ -18,6 +18,54 @@ router.get('/', async (req, res) => {
 });
 
 /**
+ * GET  Publicados<=20 && pais=Colombia
+ */
+
+router.get('/colombianos-menor20', async (req, res) => {
+  try {
+    let filters = {};
+    console.log(req.query);
+    if (req.query.pais) filters = { pais: req.query.pais };
+    const authors = await Author.find({ publicados: { $lte: 20 }, pais: 'Colombia' }, { nombre: 1, apellido: 2 });
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * GET  Autores con apellido
+ */
+
+router.get('/con-apellido', async (req, res) => {
+  try {
+    let filters = {};
+    console.log(req.query);
+    if (req.query.pais) filters = { pais: req.query.pais };
+    const authors = await Author.find({ apellido: { $exists: true } }, { nombre: 1 });
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * GET  Publicados>20 or pais=Argentina
+ */
+
+router.get('/argentinos-mayor20', async (req, res) => {
+  try {
+    let filters = {};
+    console.log(req.query);
+    if (req.query.pais) filters = { pais: req.query.pais };
+    const authors = await Author.find({$or:[{ publicados: { $gt: 20 }}, {pais: 'Argentina' }]}, { nombre: 1, apellido: 2 });
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
  * Create a new Author
  */
 router.post('/', async (req, res) => {
