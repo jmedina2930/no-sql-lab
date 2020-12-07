56b9863b8fbb389408a48e2a8c866a107c9dff2a
DESKTOP-KFJE4B7\PC <charlstore@gmail.com>
2020-12-06 19:26:35 -0500

    Laboratorio realizado por Carlos Enrique Morales Rodriguez, cc 1077420979

--- routes/authors.js
+++ routes/authors.js
@@ -17,6 +17,42 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/Consulta1_menor_20', async (req, res) => {
+  try {
+    let filters = {};
+    console.log(req.query);
+    if (req.query.pais) filters = { pais: req.query.pais };
+    const authors = await Author.find({$and:[{ publicados: { $lte: 20 }}, {pais: 'Colombia'}]}, { nombre: 1, apellido: 2 });
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.get('/Consulta2_apellido', async (req, res) => {
+  try {
+    let filters = {};
+    console.log(req.query);
+    if (req.query.pais) filters = { pais: req.query.pais };
+    const authors = await Author.find({ apellido: { $exists: true } }, { nombre: 1 });
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.get('/Consulta3_Arg_mayor20_Pub', async (req, res) => {
+  try {
+    let filters = {};
+    console.log(req.query);
+    if (req.query.pais) filters = { pais: req.query.pais };
+    const authors = await Author.find({$or:[{ publicados: { $gt: 20 }}, {pais: 'Argentina' }]}, { nombre: 1, apellido: 2 });
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 /**
  * Create a new Author
  */
